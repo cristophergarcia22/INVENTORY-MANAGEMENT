69789a33f77235e14dd87332f620540ade928814
cristophergarcia22 <cristopher.garcia@sorsu.edu.ph>
2025-12-01 20:05:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.use('/api/products', productRoutes);
 app.use('/api/suppliers', supplierRoutes);
 app.use('/api', orderRoutes);
 
+app.all('*', (req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
